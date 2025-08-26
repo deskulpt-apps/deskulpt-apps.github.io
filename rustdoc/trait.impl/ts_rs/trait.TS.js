c004da208a6436b1537896a9b66ca75a5e85b726
Charlie-XIAO <Charlie-XIAO@users.noreply.github.com>
2025-08-26 16:35:08 +0000

    deploy: CSCI-SHU-410-SE-Project/Deskulpt@938d89b07c4fe6e6cdca71bd98800a65f5250c9b

---
+++ rustdoc/trait.impl/ts_rs/trait.TS.js
@@ -0,0 +1,9 @@
+(function() {
+    var implementors = Object.fromEntries([["deskulpt_core",[["impl TS for <a class=\"enum\" href=\"deskulpt_core/config/enum.WidgetConfig.html\" title=\"enum deskulpt_core::config::WidgetConfig\">WidgetConfig</a>"],["impl TS for <a class=\"enum\" href=\"deskulpt_core/settings/enum.Theme.html\" title=\"enum deskulpt_core::settings::Theme\">Theme</a>"],["impl TS for <a class=\"struct\" href=\"deskulpt_core/settings/struct.AppSettings.html\" title=\"struct deskulpt_core::settings::AppSettings\">AppSettings</a>"],["impl TS for <a class=\"struct\" href=\"deskulpt_core/settings/struct.Shortcuts.html\" title=\"struct deskulpt_core::settings::Shortcuts\">Shortcuts</a>"],["impl TS for <a class=\"struct\" href=\"deskulpt_core/settings/struct.WidgetSettings.html\" title=\"struct deskulpt_core::settings::WidgetSettings\">WidgetSettings</a>"],["impl TS for <a class=\"struct\" href=\"deskulpt_core/struct.Settings.html\" title=\"struct deskulpt_core::Settings\">Settings</a>"]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":57,"fragment_lengths":[935]}
