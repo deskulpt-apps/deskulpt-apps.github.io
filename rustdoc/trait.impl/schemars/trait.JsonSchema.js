399fdc0ec0ce13ae8ce1203c295e435e10af4fa9
Charlie-XIAO <Charlie-XIAO@users.noreply.github.com>
2025-09-04 03:51:55 +0000

    deploy: CSCI-SHU-410-SE-Project/Deskulpt@b7366c3562296bd92e437c88660b77c133510eb7

---
+++ rustdoc/trait.impl/schemars/trait.JsonSchema.js
@@ -0,0 +1,9 @@
+(function() {
+    var implementors = Object.fromEntries([["deskulpt_core",[["impl JsonSchema for <a class=\"enum\" href=\"deskulpt_core/settings/enum.Theme.html\" title=\"enum deskulpt_core::settings::Theme\">Theme</a>"],["impl JsonSchema for <a class=\"struct\" href=\"deskulpt_core/settings/struct.AppSettings.html\" title=\"struct deskulpt_core::settings::AppSettings\">AppSettings</a>"],["impl JsonSchema for <a class=\"struct\" href=\"deskulpt_core/settings/struct.Settings.html\" title=\"struct deskulpt_core::settings::Settings\">Settings</a>"],["impl JsonSchema for <a class=\"struct\" href=\"deskulpt_core/settings/struct.Shortcuts.html\" title=\"struct deskulpt_core::settings::Shortcuts\">Shortcuts</a>"],["impl JsonSchema for <a class=\"struct\" href=\"deskulpt_core/settings/struct.WidgetSettings.html\" title=\"struct deskulpt_core::settings::WidgetSettings\">WidgetSettings</a>"]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":57,"fragment_lengths":[839]}
