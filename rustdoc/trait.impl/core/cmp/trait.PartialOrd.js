bf552e7d8f8b57e54bcbaa6be519410717e36f3c
Charlie-XIAO <Charlie-XIAO@users.noreply.github.com>
2025-09-25 09:50:25 +0000

    deploy: CSCI-SHU-410-SE-Project/Deskulpt@6f059eb95e28e2a84e500652c2527928b1ae6cc9

--- rustdoc/trait.impl/core/cmp/trait.PartialOrd.js
+++ rustdoc/trait.impl/core/cmp/trait.PartialOrd.js
@@ -1,5 +1,5 @@
 (function() {
-    var implementors = Object.fromEntries([["deskulpt_core",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.89.0/core/cmp/trait.PartialOrd.html\" title=\"trait core::cmp::PartialOrd\">PartialOrd</a> for <a class=\"enum\" href=\"deskulpt_core/settings/enum.ShortcutKey.html\" title=\"enum deskulpt_core::settings::ShortcutKey\">ShortcutKey</a>"]]]]);
+    var implementors = Object.fromEntries([["deskulpt_core",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.90.0/core/cmp/trait.PartialOrd.html\" title=\"trait core::cmp::PartialOrd\">PartialOrd</a> for <a class=\"enum\" href=\"deskulpt_core/settings/enum.ShortcutKey.html\" title=\"enum deskulpt_core::settings::ShortcutKey\">ShortcutKey</a>"]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
