a0cbab5058213ed3e2fcbcf24a0cc96334a3fadd
Charlie-XIAO <Charlie-XIAO@users.noreply.github.com>
2025-10-03 13:42:32 +0000

    deploy: CSCI-SHU-410-SE-Project/Deskulpt@4f3ed02a99cf31c7c9f2f70ddf0e700745f69fb8

--- rustdoc/trait.impl/core/fmt/trait.Display.js
+++ rustdoc/trait.impl/core/fmt/trait.Display.js
@@ -1,9 +1,9 @@
 (function() {
-    var implementors = Object.fromEntries([["deskulpt_core",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.90.0/core/fmt/trait.Display.html\" title=\"trait core::fmt::Display\">Display</a> for <a class=\"enum\" href=\"deskulpt_core/commands/error/enum.CmdError.html\" title=\"enum deskulpt_core::commands::error::CmdError\">CmdError</a>"]]],["deskulpt_specta",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.90.0/core/fmt/trait.Display.html\" title=\"trait core::fmt::Display\">Display</a> for <a class=\"enum\" href=\"deskulpt_specta/error/enum.Error.html\" title=\"enum deskulpt_specta::error::Error\">Error</a>"]]]]);
+    var implementors = Object.fromEntries([["deskulpt_core",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.90.0/core/fmt/trait.Display.html\" title=\"trait core::fmt::Display\">Display</a> for <a class=\"enum\" href=\"deskulpt_core/commands/error/enum.CmdError.html\" title=\"enum deskulpt_core::commands::error::CmdError\">CmdError</a>"]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
         window.pending_implementors = implementors;
     }
 })()
-//{"start":57,"fragment_lengths":[312,291]}
+//{"start":57,"fragment_lengths":[312]}
--- rustdoc/trait.impl/tauri_specta/trait.LanguageExt.js
+++
@@ -1,9 +0,0 @@
-(function() {
-    var implementors = Object.fromEntries([["deskulpt_specta",[["impl LanguageExt for <a class=\"struct\" href=\"deskulpt_specta/struct.TypeScript.html\" title=\"struct deskulpt_specta::TypeScript\">TypeScript</a>"]]]]);
-    if (window.register_implementors) {
-        window.register_implementors(implementors);
-    } else {
-        window.pending_implementors = implementors;
-    }
-})()
-//{"start":57,"fragment_lengths":[174]}
