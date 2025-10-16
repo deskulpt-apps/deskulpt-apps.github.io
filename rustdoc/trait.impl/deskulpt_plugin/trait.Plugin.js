7aa0bbaa1c1ec580566147df4f9892fe67d0d5c3
Charlie-XIAO <Charlie-XIAO@users.noreply.github.com>
2025-10-16 02:58:39 +0000

    deploy: deskulpt-apps/Deskulpt@7ae97d05714ada3f46e49580a7057111508b5b86

--- rustdoc/trait.impl/deskulpt_plugin/trait.Plugin.js
+++ rustdoc/trait.impl/deskulpt_plugin/trait.Plugin.js
@@ -1,9 +1,9 @@
 (function() {
-    var implementors = Object.fromEntries([["deskulpt_plugin_fs",[["impl Plugin for <a class=\"struct\" href=\"deskulpt_plugin_fs/struct.FsPlugin.html\" title=\"struct deskulpt_plugin_fs::FsPlugin\">FsPlugin</a>"]]],["deskulpt_plugin_sys",[["impl Plugin for <a class=\"struct\" href=\"deskulpt_plugin_sys/struct.SysPlugin.html\" title=\"struct deskulpt_plugin_sys::SysPlugin\">SysPlugin</a>"]]]]);
+    var implementors = Object.fromEntries([["deskulpt_plugin_fs",[["impl Plugin for <a class=\"struct\" href=\"deskulpt_plugin_fs/struct.FsPlugin.html\" title=\"struct deskulpt_plugin_fs::FsPlugin\">FsPlugin</a>"]]],["deskulpt_plugin_sys",[["impl <a class=\"trait\" href=\"deskulpt_plugin/trait.Plugin.html\" title=\"trait deskulpt_plugin::Plugin\">Plugin</a> for <a class=\"struct\" href=\"deskulpt_plugin_sys/struct.SysPlugin.html\" title=\"struct deskulpt_plugin_sys::SysPlugin\">SysPlugin</a>"]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
         window.pending_implementors = implementors;
     }
 })()
-//{"start":57,"fragment_lengths":[172,179]}
+//{"start":57,"fragment_lengths":[172,285]}
